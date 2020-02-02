7a0f583c5b7b724f9e58e17da6becfa0efa40011
kazukichi <zexyl_x@icloud.com>
2020-02-03 00:29:40 +0900

    ひとまず例取得、nodemon yarn add

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // バックエンドサーバの目的(JSON形式の生データをフロントエンドに提供)
 
-// Nodeの組み込みWebサーバモジュールをインポート
-const http = require('http');
+// Expressアプリケーションを作成するために使用される関数
+const express = require('express');
+const app = express();
 
 let notes = [
   {
-    id: 1,
+    id: 6463,
     content: 'HTML is easy',
     date: '2019-05-30T17:30:31.098Z',
     important: true
@@ -24,14 +25,25 @@ let notes = [
   }
 ];
 
-// httpモジュールのcreateServerメソッド(webサーバ作成)
-const app = http.createServer((request, response) => {
-  // コード200で返される
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(notes));
+// // httpモジュールのcreateServerメソッド(webサーバ作成)
+// const app = http.createServer((request, response) => {
+//   // コード200で返される
+//   response.writeHead(200, { 'Content-Type': 'application/json' });
+//   response.end(JSON.stringify(notes));
+// });
+
+app.get('/', (req, res) => {
+  // send(stringを含む応答を送信)
+  res.send('<h1>Hello World!!!</h1>');
 });
 
-const port = 3001;
+app.get('/notes', (req, res) => {
+  // JSON形式の文字列として渡されたnotes配列が送信
+  res.json(notes);
+});
+
+const PORT = 3001;
 // appサーバをバインドしHTTPリクエスト要求をこなす
-app.listen(port);
-console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
